837e415dcb38ad365122b9c644edbb17427b4d83
Foxy5410 <simono5410@gmail.com>
2025-02-27 08:04:24 +0100

    modified:   keyboards/charlotte_v1/keyboard.json 	new file:   keyboards/charlotte_v1/post_rules.mk 	modified:   keyboards/charlotte_v1/rules.mk 	new file:   keyboards/scottokeebs/scotto34/.vscode/c_cpp_properties.json 	new file:   keyboards/scottokeebs/scotto34/.vscode/launch.json 	new file:   keyboards/scottokeebs/scotto34/.vscode/settings.json

--- keyboards/charlotte_v1/rules.mk
+++ keyboards/charlotte_v1/rules.mk
@@ -1,13 +1,4 @@
 LTO_ENABLE = yes
 
-OS_DETECTION_ENABLE = yes
-SPACE_CADET_ENABLE = no
-GRAVE_ESC_ENABLE = no 
-MAGIC_ENABLE = no
-
 OLED_DRIVER = ssd1306
 OLED_TRANSPORT = i2c
-
-ifeq ($(strip $(OLED_ENABLE)), yes)
-    SRC += oled_display.c
-endif
